cce10665869c3317e7c62a0f3ea1ad1055a88022
ma-zormati <zormati@ieee.org>
2023-06-16 16:05:32 +0200

    added topology v1

--- spleaf.py
+++ spleaf.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python
+
 from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.node import RemoteController
+
+REMOTE_CONTROLLER_IP="172.17.0.2"
 
 class SpineLeaf ( Topo ):
     "Spine Leaf Topology S2 L4 H4/L"
 
-    def build(self):
+    def __init__(self):
         #Initialize topology
+        Topo.__init__(self)
         switchesSpine=[]
         switchesLeaf=[]
         hosts=[]
@@ -38,3 +47,12 @@ class SpineLeaf ( Topo ):
 
 
 topos = {'spineleaf': (lambda : SpineLeaf())}
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo=SpineLeaf()
+    net=Mininet(topo=topo, controller=None, autoStaticArp=True)
+    net.addController("c0", controller=RemoteController, ip=REMOTE_CONTROLLER_IP)
+    net.start()
+    CLI(net)
+    net.stop()
